27372a46b908b927ad2e4e94d9bedf7d1c6e373b
Conor O'Brien <Conor.O'Brien@astronics.com>
2024-04-25 14:20:34 -0700

    added a simple title

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Title</title>
+    <title>Testing Annotated PDF Queries</title>
 </head>
 <body>
     <pre><code><?php include 'get_assn.php'; ?></code></pre>
