e0953a0295955f20384c95e22a7e1d83fb5a0efa
Conor O'Brien <91850829+conorep@users.noreply.github.com>
2024-04-25 14:29:47 -0700

    Update test_creds.php

--- creds/test_creds.php
+++ creds/test_creds.php
@@ -1,10 +1,9 @@
 <?php
 
-// using Vergil Gunch with ANT101 for instructor tests
 $canURL = 'https://qwickly.instructure.com';
-$cID = 17; //course id
-$assnID = 31; //anthropology paper
-$rubID = 105; //rubric ID
-$uToken = '12162~ahfx9dvQA8aUZLEClXy163FCCNmNG9XVbmnB5ze3JVEgOESglAcRVN3gZbmJP4ze';
-$stuToken = '12162~CTIl7K1eij1mEgBoFXzHAoMcVRjuSzoqlu7ywWHt2f8U8aLl5QWBmTmdQws9VXnM';
-$stuID = 255; //Albert Maltz
+$cID = 0; //course id
+$assnID = 0; //assignment paper
+$rubID = 0; //rubric ID
+$uToken = 'TOKEN ID (INSTRUCTOR)';
+$stuToken = 'TOKEN ID (STUDENT)';
+$stuID = 0; //student ID
