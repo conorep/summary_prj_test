164f2ce9ad6542948dac42ea0cef8098d02daf2b
Conor O'Brien <conorepobrien@gmail.com>
2024-04-24 21:54:18 -0700

    got closer to final 'get this annotated pdf' call - altered functions to allow the test print OR just 'retrieve data' - starting to build queries that get to the actual process of building and retrieving the annotated PDF

--- get_assn.php
+++ get_assn.php
@@ -22,31 +22,55 @@ getReqdData();
 $headers[0] = "Authorization: Bearer $stuToken";
 $stuID = 'self';
 echo "\n\n\n\n\n\n<h3><u>STUDENT - SUSAN B ANTHONY, ANT101</h3></u>\n\n";
-getReqdData();
+//getReqdData();
 
 function printRes($method, $url, $data, $printStatement) {
-    global $headers;
-
-    $assnData = apiCall($method, $url, $data, $headers);
-    $assnData = json_decode($assnData);
+    $assnData = returnRes($method, $url, $data);
     echo "$printStatement:\n\n";
     print_r($assnData);
     echo "\n\n";
+}
+
+function returnRes($method, $url, $data, $extraOpts = null, $jsonEncodeIt = true) {
+    global $headers;
+
+    $assnData = apiCall($method, $url, $data, $headers, $extraOpts);
+    if($jsonEncodeIt) {
+        return json_decode($assnData);
+    }
+    return $assnData;
 }
 
 function getReqdData() {
 	global $canURL, $cID, $assnID, $rubID, $stuID;
 
     echo "<u>Get a single assignment download:</u>\n";
-	$getSub = $canURL."api/v1/courses/$cID/assignments/$assnID/submissions/$stuID".
+	$getSub = $canURL."/api/v1/courses/$cID/assignments/$assnID/submissions/$stuID".
         "?include[]=assignment&include[]=full_rubric_assessment";
-	printRes('GET', $getSub, NULL, 'ASSIGNMENT DATA');
+	//printRes('GET', $getSub, NULL, 'ASSIGNMENT DATA');
 
-    echo "<u>Get a single assignment rubric:</u>\n";
-    $getRub = $canURL."api/v1/courses/$cID/rubrics/$rubID";
-	printRes('GET', $getRub, NULL, 'RUBRIC DATA');
+    // get from res... .attachments.preview_url
+    $resURL = returnRes('GET', $getSub, NULL);
+    $thisResURL = $resURL['attachments']['preview_url'];
+    $getPreview = $canURL.$thisResURL;
 
-    echo "<u>Get all rubrics from course:</u>\n";
-    $getRubs = $canURL."api/v1/courses/$cID/rubrics";
-	printRes('GET', $getRubs, NULL, 'ALL RUBRIC DATA');
+    $theRedirect = returnRes('GET', $getPreview, NULL, [CURLOPT_FOLLOWLOCATION => 0], false);
+    print_r($theRedirect);
+    $htmlReturn = explode('href="', $theRedirect)[1];
+    $htmlReturn = explode('">', $htmlReturn)[0];
+    $htmlReturn = explode('/view', $htmlReturn)[0];
+    $htmlReturn .= 'annotated.pdf';
+
+    //now post to the htmlReturn redirect URL. should get 'Accepted' as return data.
+    $startAnnoBuild = returnRes('POST', $htmlReturn, NULL);
+    echo($startAnnoBuild);
+
+
+//    echo "<u>Get a single assignment rubric:</u>\n";
+//    $getRub = $canURL."/api/v1/courses/$cID/rubrics/$rubID";
+//	printRes('GET', $getRub, NULL, 'RUBRIC DATA');
+//
+//    echo "<u>Get all rubrics from course:</u>\n";
+//    $getRubs = $canURL."/api/v1/courses/$cID/rubrics";
+//	printRes('GET', $getRubs, NULL, 'ALL RUBRIC DATA');
 }
