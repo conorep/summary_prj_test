f49c79d1c34e4a51dd8b6eea6a8832100848bd58
Conor O'Brien <Conor.O'Brien@astronics.com>
2024-04-30 16:09:44 -0700

    added babel to handle typescript

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  
-  <link rel='stylesheet' href='styles/styling.css'>
+  <meta charset='utf-8' />
+  <script src='https://unpkg.com/babel-standalone@6/babel.min.js'></script>
+  <link rel='stylesheet' href='styles/styling.css?v=<?php echo filemtime('styles/styling.css');?>'>
   <title>Testing Annotated PDF Queries</title>
 </head>
 <body>
   <div id='mainCont'>
-    <?php include 'get_assn.php'; ?>
+	  <?php include 'get_assn.php'; ?>
   </div>
-  <script type='application/javascript' src='scripts/scripting.tsx'></script>
+  <script type='text/babel' src='scripts/scripting.tsx?v=<?php echo filemtime('scripts/scripting.tsx');?>'></script>
 </body>
 </html>
